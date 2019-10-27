32cfdebd9cae5303c453949294802105bc0b53e8
Dieter Jackson <djackcu@gmail.com>
2019-10-27 16:12:06 +0100

    style: Changing style of dictionaries elements

--- src/components/dictionaryElement/dictionaryElement.js
+++ src/components/dictionaryElement/dictionaryElement.js
@@ -3,7 +3,10 @@ import React from 'react'
 function DictionaryElement({dictionary}) {
     return (
         <div className="dictionary-element">
-            {dictionary}
+            <div className="dictionary-element_name">{dictionary}</div>
+            <div className="dictionary-element_description">Description</div>
+            <div className="dictionary-element_btn_validate">Validate</div>
+            <div className="dictionary-element_btn_delete">X</div>
         </div>
     )
 }
