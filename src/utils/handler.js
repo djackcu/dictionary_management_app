8a99a1a4233c56d6df61e0305a101f0b1628b26f
Dieter Jackson <djackcu@gmail.com>
2019-10-29 05:57:54 +0100

    feat: Validating cycle and chain

--- src/utils/handler.js
+++ src/utils/handler.js
@@ -1,14 +1,9 @@
-export class Dictionary{
+export class Graph{
     constructor(name='dictionary',description = 'Default description') {
-        this._data = new Map();
+        this._adjList = new Map();
         this._name = name;
         this._description = description;
-
         }
-    get size(){
-        return this._dictionary.size;
-    }
-
     get name(){
         return this._name;
     }
@@ -17,28 +12,61 @@ export class Dictionary{
         return this._description;
     }
 
-    get data(){
-        return this._data;
+    addVertex(vertex) {
+        if (!this._adjList.has(vertex)) {
+            this._adjList.set(vertex, new Set())
+        };
     }
-
-    validateDictionary(){
+    addEdge(vertex1,vertex2) {
+        this._adjList.get(vertex1).add(vertex2);
+    }
+    dfs() {
+        const visited = {}
+        for (let node of this._adjList.keys()) {
+        this._dfsUtil(node, visited)
+        }
+    }
         
+    _dfsUtil(vertex, visited) {
+        if (!visited[vertex]){  
+        visited[vertex] = true
+        const neighbors = this._adjList.get(vertex);
+        for (let neighbor of neighbors.keys()) {
+            console.log(vertex, neighbor)
+            this._dfsUtil(neighbor, visited)
+        
+        }
+        }
     }
 
-    validateDuplicates(){
-        return false;
+    detectCycle() {
+        const visited = {};
+        const recStack = {};
+        const chain = [];
+        
+        for (let node of this._adjList.keys()) {
+        if (this._detectCycleUtil(node, visited , recStack,chain)) 
+            return 'there is a cycle'
+        }
+        return 'no cycle!'
     }
 
-    validateForks(){
+    _detectCycleUtil(vertex, visited, recStack,chain) {
+        if(!visited[vertex]){
+        visited[vertex] = true;
+        recStack[vertex] = true;
+        const nodeNeighbors = this._adjList.get(vertex);
+        for(let currentNode of nodeNeighbors.keys()){
+            console.log('parent', vertex, 'Child',currentNode,'chain',chain);
+            if(this._adjList.get(currentNode).size > 0) chain.push(currentNode);
+            if(!visited[currentNode] && this._detectCycleUtil(currentNode,visited, recStack,chain)){
+            return true;
+            } else if (recStack[currentNode]){
+            return true;
+            }
+        }
+        }
+        recStack[vertex] = false;
         return false;
     }
-
-    validateCycles(){
-        return false;
-    }
-    
-    validateChains(){
-        return false;
-    }
-    
 }
