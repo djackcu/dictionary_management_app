43db426edd6d64d3b6bcceff9279336291449d4f
Dieter Jackson <djackcu@gmail.com>
2019-11-11 22:26:44 +0100

    refactor: Clean console

--- src/redux/reducers/dictionariesReducer.js
+++ src/redux/reducers/dictionariesReducer.js
@@ -15,17 +15,14 @@ export default function dictionariesReducer(state = initialState.dictionaries, a
 
     case types.ADD_ROW:
         const addDictionaryRow = new Dictionary(action.dictionary).addRow(action.range,action.domain);
-        console.log(addDictionaryRow, action.dictionary.dataList);
         return[...state.map(dict => {return (dict.name === addDictionaryRow.name)?addDictionaryRow:dict})];
 
     case types.DELETE_ROW:
         const deleteDictionaryRow = new Dictionary(action.dictionary).deleteRow(action.range,action.domain);
-        console.log(deleteDictionaryRow, action.dictionary.dataList);
         return[...state.map(dict => {return (dict.name === deleteDictionaryRow.name)?deleteDictionaryRow:dict})];
 
     case types.UPDATE_ROW:
         const updateDictionaryRow = new Dictionary(action.dictionary).updateRow(action.range,action.domain);
-        console.log(updateDictionaryRow, action.dictionary.dataList);
         return[...state.map(dict => {return (dict.name === updateDictionaryRow.name)?updateDictionaryRow:dict})];
 
     default:
